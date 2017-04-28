bb4726906ad2025bfe548dabdf2d792dcbcb0c09
Sam Murphy <samsammurphy@gmail.com>
2017-04-28 13:20:00 -1000

    run6S() accepts any Py6S wavelength -> updates .csv

--- bin/atmcorr.py
+++ bin/atmcorr.py
@@ -12,9 +12,11 @@ https://github.com/samsammurphy/gee-atmcorr-S2-batch
 
 """
 import ee
+import sys
 from atmospheric import Atmospheric
 from astronomical import Astronomical
 import pandas as pd
+import numpy as np
 import math
 import time
 from Py6S import *
@@ -46,20 +48,33 @@ class Atmcorr:
   
   def exportInputs(fc_with_inputs):
     """
-    Export atmcorr inputs to .csv file, i.e. allows batch
+    Export atmcorr inputs to .csv file. this allows 'batch'
     processing by avoiding repeated .getInfo() calls
     """
     task = ee.batch.Export.table.toDrive(collection = fc_with_inputs,\
                                   description = 'exporting atmcorr inputs to csv..',\
-                                  fileNamePrefix = 'atmcorr'
+                                  fileNameprefix = 'atmcorr'
                                   )
     return task
-
-  def run6S(csv_path):
+    
+  def run6S(csv_path, predefined=None, start_wavelength=None, end_wavelength=None, spectral_filter=None):
 
     print('running 6S..')
     
     t = time.time()
+
+    # wavelength (predefined or user-defined ?)
+    if predefined:
+      wavelength = Wavelength(PredefinedWavelengths.__dict__[predefined])
+      prefix = predefined # <-- i.e. prefix for column name for .csv (e.g. 'S2A_MSI_01')
+    if start_wavelength:
+      wavelength = Wavelength(start_wavelength, end_wavelength=end_wavelength, filter=spectral_filter)
+      prefix = ['w',str(start_wavelength)]
+      if end_wavelength:
+        prefix.append(str(end_wavelength))
+      if spectral_filter:
+        prefix.append('f')
+      prefix = '_'.join(prefix)
 
     # initiate 6S object with constants
     s = SixS()
@@ -69,6 +84,7 @@ class Atmcorr:
     s.geometry.view_z = 0
     s.geometry.month = 1 # Earth-sun distance correction is later
     s.geometry.day = 4   # applied from perihelion, i.e. Jan 4th.  
+    s.wavelength = wavelength
     
     # run 6S over input variables
     inputs = pd.read_csv(csv_path)
@@ -81,18 +97,12 @@ class Atmcorr:
       s.aot550 = inputs['AOT'][i]
       s.altitudes.set_target_custom_altitude(inputs['altitude'][i])
       
-      """
-      Py6S user interface!
-      """
-      #s.wavelength = spectrum
-      
       # run 6S
       s.run()
       
       # solar irradiance
       Edir = s.outputs.direct_solar_irradiance             # direct solar irradiance
       Edif = s.outputs.diffuse_solar_irradiance            # diffuse solar irradiance
-      E = Edir + Edif                                      # total solar irraduance
       # transmissivity
       absorb  = s.outputs.trans['global_gas'].upward       # absorption transmissivity
       scatter = s.outputs.trans['total_scattering'].upward # scattering transmissivity
@@ -101,35 +111,19 @@ class Atmcorr:
       Lp   = s.outputs.atmospheric_intrinsic_radiance      # path radiance
       # correction coefficients for this configuration
       a = Lp
-      b = (tau2*E)/math.pi
-      outputs.append((Edir, Edif, tau2, Lp, a, b))
-
-      """
-      list comprehension to add outputs to df then export
-
-      dadaa!  ?   :)
-      """
-   
+      b = (tau2*(Edir + Edif))/math.pi
+      outputs.append((a, b))
+    
     print('time check {}'.format(time.time()-t))
+    
+    # add to dataframe
+    inputs[prefix+'_6S_a'] = [x[0] for x in outputs]
+    inputs[prefix+'_6S_b'] = [x[1] for x in outputs]
 
-"""
-Step 1
-"""
-# from testsites import Testsites
-# testsites = Testsites()
-# fc = testsites.get()
-# fc_with_inputs = Atmcorr.findInputs(fc)
-# task = Atmcorr.exportInputs(fc_with_inputs)
-# task.start()
+    # export updated dataframe to .csv
+    outfile = os.path.basename(csv_path)[:-4]+'_updated.csv'
+    outpath = os.path.join(os.path.dirname(csv_path),outfile)
+    inputs.to_csv(outpath)
 
-"""
-Step 2
-"""
-# download the csv file locally 
-Atmcorr.run6S('testsites.csv')
-
-"""
-Step 3
-"""
-# upload to fusion table
-# use in GEE
+# e.g.
+# Atmcorr.run6S('testsites.csv', predefined = 'S2A_MSI_01')
